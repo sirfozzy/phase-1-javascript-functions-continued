af5345420abf0f305dadfb51a22b90e06c326e53
Jason Antman <antmanjt@gmail.com>
2022-03-20 10:24:02 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(act='roller-skate'){
+    return `This Saturday, I want to ${act}!`;
+}
+
+function mondayWork(act1='go to the office'){
+    return `This Monday, I will ${act1}.`
+
+}
+
+function wrapAdjective(defaul='*'){
+    return function inner(special='*'){
+        return `You are ${defaul}${special}${defaul}!`
+    }
+}
+
+wrapAdjective("||")("a dedicated programmer")
